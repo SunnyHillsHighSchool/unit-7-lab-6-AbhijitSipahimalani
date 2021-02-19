6683eabb40c0503e232c789e49f0d89148295ec5
Abhijit Sipahimalani <800022909@fjuhsd.org>
2021-02-19 23:28:33 +0000

    writing methods

--- Word.java
+++ Word.java
@@ -8,14 +8,36 @@ public class Word
 {
    private static String vowels = "AEIOUaeiou";   	//why is this here?
    																//why is it static?
-   
+   private String word;
    //add an instance variable
-
+int count = 0;
 	//create a constructor
-	
-	//create other methods
+public word ()
+{
+  word ="";
+}	
+public static void getNumVowels() {
+  for(int i = 0; i < word.length(); i++){
+     if(word.valueOf(i) == "a"  || word.valueOf(i) ==  "A" || word.valueOf(i) ==  "e" || word.valueOf(i) ==  "E" || word.valueOf(i) ==  "i" || word.valueOf(i) ==  "I" || word.valueOf(i) ==  "o" || word.valueOf(i) ==  "O" || word.valueOf(i) ==  "u" || word.valueOf(i) ==   "U") {
+    count += 1;
+    
+     }
+  }
+  return count;
+}
 	//look at the sample runner code
 	//on the lab handout
 
 	//create a toString method
+public String toString(){
+  return Integer.toString(count);
+
+}
+public String getWord (){
+  return word;
+}
+public void setWord (String s){
+  word = s;
+}
+
 }
